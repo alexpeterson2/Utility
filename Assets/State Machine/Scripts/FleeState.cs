9dba45010d9e971d887f8edb13dc42f024823b47
alexpeterson2 <35349356+alexpeterson2@users.noreply.github.com>
2019-12-09 20:03:21 -0700

    Added Standard assets
    Imported Unity Standard assets from the store and remade scripts and prefabs from Dan's 400 state machine tutorial with some modifications. AICharacterController.cs in Standard assets had to be slightly altered to work with some scripts.

---
+++ Assets/State Machine/Scripts/FleeState.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FleeState : State {
+
+    Transform destination;
+    GameObject fleeTo;
+
+    public FleeState(StateController stateController) : base(stateController) { }
+
+    public override void CheckTransitions()
+    {
+        if (!stateController.CheckIfInRange("Player"))
+        {
+            stateController.SetState(new PatrolState(stateController));
+        }
+    }
+    public override void Act()
+    {
+        if (stateController.enemyToFlee != null)
+        {
+            if (GameObject.FindGameObjectsWithTag("flee") != null)
+            {
+                foreach (GameObject f in GameObject.FindGameObjectsWithTag("flee"))
+                {
+                    stateController.DestroyObject(f);
+                }
+            }
+
+            fleeTo = new GameObject();
+            fleeTo.tag = "flee";
+
+            fleeTo.transform.position = (stateController.enemyToFlee.transform.position + stateController.transform.position) * 2;
+            stateController.ai.SetTarget(fleeTo.transform);
+        }
+    }
+    public override void OnStateEnter()
+    {
+        
+        stateController.ChangeColor(Color.yellow);
+        stateController.ai.agent.speed = 1.5f;
+    }
+}
